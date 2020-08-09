c9f64a48e7d7239324d10a98b8f7a80e6600f1ca
kimminsuk85 <kimminsuk85@ncsoft.com>
2020-08-09 17:22:22 +0900

    producer 누라된 ENABLE_IDEMPOTENCE_CONFIG 설정 추가

--- transaction-generator/src/main/java/com/kakaobank/infra/GeneratorProducer.java
+++ transaction-generator/src/main/java/com/kakaobank/infra/GeneratorProducer.java
@@ -47,7 +47,8 @@ public final class GeneratorProducer implements BankProducer<RawEvent> {
 		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 		properties.put(ProducerConfig.ACKS_CONFIG, "all");
-		properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1");
+		properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+		properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 		properties.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
 		properties.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "bank.transactions");
 		properties.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 10000);
